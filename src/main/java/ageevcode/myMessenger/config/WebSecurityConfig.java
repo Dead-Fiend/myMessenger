8d6eaccd8d50c884a0bf17a6ea32a7715bf82108
Dead-Fiend <83966220+Dead-Fiend@users.noreply.github.com>
2021-12-08 22:11:25 +0300

    upd

--- src/main/java/ageevcode/myMessenger/config/WebSecurityConfig.java
+++ src/main/java/ageevcode/myMessenger/config/WebSecurityConfig.java
@@ -1,18 +1,34 @@
 package ageevcode.myMessenger.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .mvcMatchers("/").permitAll()
-                .anyRequest().authenticated()
+                .antMatchers("/", "/login**", "/js/**", "/error**").permitAll()
+                .anyRequest().authenticated();
+        http
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
                 .and()
-                .csrf().disable();
+                .logout()
+                .permitAll();
+    }
+    @Configuration
+    protected static class AuthenticationConfiguration extends GlobalAuthenticationConfigurerAdapter {
+        @Override
+        public void init(AuthenticationManagerBuilder auth) throws Exception {
+            auth
+                    .inMemoryAuthentication()
+                    .withUser("user").password("password").roles("USER");
+        }
     }
 }
