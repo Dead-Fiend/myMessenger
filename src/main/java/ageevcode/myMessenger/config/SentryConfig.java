0a800a6b391f47005efa51c0fd2d6818a7640d4d
Dead-Fiend <83966220+Dead-Fiend@users.noreply.github.com>
2022-01-15 18:11:58 +0300

    upd

--- src/main/java/ageevcode/myMessenger/config/SentryConfig.java
+++ src/main/java/ageevcode/myMessenger/config/SentryConfig.java
@@ -1,10 +1,14 @@
 package ageevcode.myMessenger.config;
 
 import io.sentry.spring.EnableSentry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+
+
 
 @Configuration
 @EnableSentry
 /*@EnableSentry(dsn = "https://47bd6a422fa147b6a9a9957d1b8d2ef5@o1112425.ingest.sentry.io/6142012")*/
+//SENTRY_DSN
 public class SentryConfig {
 }
