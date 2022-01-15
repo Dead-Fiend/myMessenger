207b86a5242836d2f320de7d4f47f0e77dd6faa2
Dead-Fiend <83966220+Dead-Fiend@users.noreply.github.com>
2022-01-15 18:57:27 +0300

    upd

--- src/main/java/ageevcode/myMessenger/config/WebSecurityConfig.java
+++ src/main/java/ageevcode/myMessenger/config/WebSecurityConfig.java
@@ -49,7 +49,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
                 .userDetailsService(userService)
-                .passwordEncoder(passwordEncoder);
+                .passwordEncoder(getPasswordEncoder());
 /*                .usersByUsernameQuery("select username, password, active from usr where username=?")
                 .authoritiesByUsernameQuery("select u.username, ur.roles from usr u inner join user_role ur on u.id = ur.user_id where u.username=?");*/
 
