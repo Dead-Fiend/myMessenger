f5c6e6819a5d81de0f22ac4fe2f67b67fd33b1df
Dead-Fiend <83966220+Dead-Fiend@users.noreply.github.com>
2022-01-15 19:29:46 +0300

    upd

--- src/main/java/ageevcode/myMessenger/MyMessengerApplication.java
+++ src/main/java/ageevcode/myMessenger/MyMessengerApplication.java
@@ -1,6 +1,5 @@
 package ageevcode.myMessenger;
 
-import io.sentry.Sentry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/main/java/ageevcode/myMessenger/config/WebSecurityConfig.java
+++ src/main/java/ageevcode/myMessenger/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import ageevcode.myMessenger.service.ProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,9 +15,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Lazy
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @Lazy
     @Autowired
     private ProfileService userService;
 
