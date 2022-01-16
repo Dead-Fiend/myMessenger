bee3d81d167e5a685f6a80c282949dfbc31b5503
Dead-Fiend <83966220+Dead-Fiend@users.noreply.github.com>
2022-01-16 16:00:57 +0300

    upd

--- src/main/java/ageevcode/myMessenger/MyMessengerApplication.java
+++ src/main/java/ageevcode/myMessenger/MyMessengerApplication.java
@@ -1,11 +1,13 @@
 package ageevcode.myMessenger;
 
+import io.sentry.Sentry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MyMessengerApplication {
     public static void main(String[] args) {
+        Sentry.captureMessage("Application started");
         SpringApplication.run(MyMessengerApplication.class, args);
     }
 }
