f1499282236c0553b55e3efea5e13e3e679b6107
Dead-Fiend <83966220+Dead-Fiend@users.noreply.github.com>
2022-02-04 14:07:02 +0300

    upd

--- src/main/resources/js/components/messages/MessageRow.vue
+++ src/main/resources/js/components/messages/MessageRow.vue
@@ -1,5 +1,7 @@
 <template>
-  <v-card class="my-2">
+<v-layout row>
+  <v-flex v-if="!(profile.id == message.author.id)">
+    <v-card class="my-2" width="200">
     <v-card-text class="text--primary">
       <user-link :user="message.author" size="48"></user-link>
       <div class="pt-3">{{message.text}}</div>
@@ -13,16 +15,38 @@
       </v-btn>
     </v-card-actions>
   </v-card>
+  </v-flex>
+
+  <v-flex v-if="profile.id == message.author.id">
+    <v-card class="ml-auto" width="200">
+      <v-card-text class="text--primary">
+        <user-link :user="message.author" size="48"></user-link>
+        <div class="pt-3">{{message.text}}</div>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn icon @click="edit" small>
+          <v-icon>edit</v-icon>
+        </v-btn>
+        <v-btn icon @click="del" small>
+          <v-icon>close</v-icon>
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-flex>
+</v-layout>
 </template>
 
 <script>
 import UserLink from "components/UserLink.vue";
-import {mapActions} from "vuex";
+import {mapActions, mapGetters, mapState} from "vuex";
 
 export default {
   name: "MessageRow",
   components: {
     UserLink
+  },
+  computed: {
+    ...mapState(['profile']),
   },
   props: ['message', 'editMessage', 'redact'],
   methods: {
--- src/main/resources/js/pages/Messenger.vue
+++ src/main/resources/js/pages/Messenger.vue
@@ -1,25 +1,25 @@
 <template>
-  <v-container column class="mx-2">
-    <v-flex class="title mb-3">
-      <div>Привет, {{profile.username}} </div><div>{{chat}}</div>
-    </v-flex>
-    <v-flex v-if="(this.$route.params.id) === undefined">
-      <chat-row v-for="chat in chats"
-                :key="chat.id"
-                :chat="chat"
-      ></chat-row>
-    </v-flex>
-    <v-flex v-if="(this.$route.params.id) !== undefined">
-      <v-layout align-space-around justify-start column>
-        <message-form :messageAttr="message" :redact="redact"/>
-        <message-row v-for="message in (sortedMessages.filter(n => n.interlocutor.id == this.$route.params.id).concat(sortedMessages.filter(n => n.author.id == this.$route.params.id)))"
-                     :key="message.id"
-                     :message="message"
-                     :editMessage="editMessage"
-                     :redact="redact"/>
-      </v-layout>
-    </v-flex>
-  </v-container>
+    <v-layout column class="mx-8">
+      <v-flex class="title mb-3">
+        <div>Привет, {{profile.username}} </div><div>{{chat}}</div>
+      </v-flex>
+      <v-flex v-if="(this.$route.params.id) === undefined">
+        <chat-row v-for="chat in chats"
+                  :key="chat.id"
+                  :chat="chat"
+        ></chat-row>
+      </v-flex>
+      <v-flex v-if="(this.$route.params.id) !== undefined">
+        <v-layout align-space-around justify-start column>
+          <message-form :messageAttr="message" :redact="redact"/>
+          <message-row class="my-2" v-for="message in (sortedMessages.filter(n => n.interlocutor.id == this.$route.params.id).concat(sortedMessages.filter(n => n.author.id == this.$route.params.id)))"
+                       :key="message.id"
+                       :message="message"
+                       :editMessage="editMessage"
+                       :redact="redact"/>
+        </v-layout>
+      </v-flex>
+    </v-layout>
 </template>
 
 <script>
